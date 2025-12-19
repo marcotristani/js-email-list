65802af4504a13d5ee610f7762f7f4f9cdd7a4df
marcotristani <marco.tristani@hotmail.it>
2025-12-19 15:39:18 +0100

    flat: generalizzata funzione a n mail

--- js/script.js
+++ js/script.js
@@ -3,19 +3,19 @@ const listOutput = document.getElementById("list-output");
 const buttonRefresh = document.getElementById("btn");
 
 //Chiamo la funzione per stampare 10 mail
-emailRandom();
+emailRandom(10);
 
 //aggiungo evento quando viene cliccato bottone
-buttonRefresh.addEventListener("click", emailRandom);
+buttonRefresh.addEventListener("click", () => emailRandom(10));
 
 //Definisco la funzione che mi stamperà n email in lista richiamando l'API
-function emailRandom() {
+function emailRandom(n) {
 
   //Dichiaro endpoint con indirizzio API
   const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail";
 
   listOutput.innerHTML = "";// Inizializzo l'output nel html come stringa vuota così quandio vado a premere il bottone per ricaricare non mi aggiungerà email alla lista già presente
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < n; i++) {
     //Eseguo chiamata API
     axios.get(endpoint)
       .then((response) => {
